031b49ca965427e92296063d31a28f3b29574161
gongming <gongmingqm10@foxmail.com>
2014-07-11 12:39:57 +0800

    List book details

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,16 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.dao.BookReader;
+import com.twu.biblioteca.model.Book;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        BookReader reader = new BookReader("books.csv");
+        int i = 1;
+        for (Book book : reader.listBooks()) {
+            System.out.println( i++ + ". " +book.getName() + ", " + book.getAuthor() + ", " + book.getPublishYear());
+        }
     }
 }
